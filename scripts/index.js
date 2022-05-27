52a2627bd61c42c95890375c60dc4673164f8103
Alexander Vizniy <senak98@yandex.ru>
2022-05-27 22:32:54 +0300

    feature: final project version

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+let editBtn = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+
+let pofileName = document.querySelector('.profile__name');
+
+let pofileJob = document.querySelector('.profile__job');
+
+let inputName = document.querySelector('.popup__input_type_name');
+let inputjob = document.querySelector('.popup__input_type_job');
+
+let popupClose = popup.querySelector('.popup__close');
+
+let popupSave = popup.querySelector('.popup__save');
+
+inputName.value = pofileName.textContent;
+inputjob.value = pofileJob.textContent;
+
+function popupOpener(popupElement) {
+  popupElement.classList.add('popup__isopen');
+}
+
+editBtn.addEventListener('click', function() {
+  popupOpener(popup);
+});
+
+function popupCloser(popupElement) {
+  popupElement.classList.remove('popup__isopen');
+}
+
+popupClose.addEventListener('click', function() {
+  popupCloser(popup);
+  inputName.value = pofileName.textContent;
+  inputjob.value = pofileJob.textContent;
+})
+
+popupSave.addEventListener('click', function (event) {
+  event.preventDefault();
+  pofileName.textContent = inputName.value;
+  pofileJob.textContent = inputjob.value;
+  popupCloser(popup);
+})
+
+
