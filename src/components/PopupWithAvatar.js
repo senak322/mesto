f0f430a5a517c92da47227e251d7f6725ad0cb2a
Alexander Vizniy <senak98@yandex.ru>
2022-09-17 00:38:16 +0300

    feat: add popup with avatar

---
+++ src/components/PopupWithAvatar.js
@@ -0,0 +1,22 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithAvatar extends Popup {
+  constructor(selector, handleDelete) {
+    super(selector)
+    this._handleDelete = handleDelete,
+    this._deleteBtn = this.popup.querySelector('.popup__save_type_delete')
+
+  }
+
+  setElem(item, cardId) {
+    this._cardId = cardId;
+    this._item = item
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._deleteBtn.addEventListener('click', () => {
+      this._handleDelete(this._item, this._cardId)
+    })
+  }
+}
